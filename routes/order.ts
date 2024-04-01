8e868756af3a366d4d0f4db0b757104a884b1cb5
Andre Sousa <up202005277@up.pt>
2024-04-01 19:02:01 +0100

    Steal someone else’s personal data without using Injection

--- routes/order.ts
+++ routes/order.ts
@@ -158,7 +158,7 @@ module.exports = function placeOrder () {
             addressId: req.body.orderDetails ? req.body.orderDetails.addressId : null,
             orderId,
             delivered: false,
-            email: (email ? email.replace(/[aeiou]/gi, '*') : undefined),
+            email,
             totalPrice,
             products: basketProducts,
             bonus: totalPoints,
