698e404d3c597d18d42047883f7eff3ec62466b5
Andre Sousa <up202005277@up.pt>
2024-04-01 15:02:42 +0100

    Access a confidential document

--- routes/fileServer.ts
+++ routes/fileServer.ts
@@ -8,11 +8,16 @@ import { type Request, type Response, type NextFunction } from 'express'
 import challengeUtils = require('../lib/challengeUtils')
 
 import * as utils from '../lib/utils'
+
 const security = require('../lib/insecurity')
 const challenges = require('../data/datacache').challenges
+const allowList = ['legal.md', 'incident-support.kdbx']
 
 module.exports = function servePublicFiles () {
-  return ({ params, query }: Request, res: Response, next: NextFunction) => {
+  return ({
+    params,
+    query
+  }: Request, res: Response, next: NextFunction) => {
     const file = params.file
 
     if (!file.includes('/')) {
@@ -24,28 +29,38 @@ module.exports = function servePublicFiles () {
   }
 
   function verify (file: string, res: Response, next: NextFunction) {
-    if (file && (endsWithAllowlistedFileType(file) || (file === 'incident-support.kdbx'))) {
+    if (file && allowList.includes(file) && (endsWithAllowlistedFileType(file))) {
       file = security.cutOffPoisonNullByte(file)
 
-      challengeUtils.solveIf(challenges.directoryListingChallenge, () => { return file.toLowerCase() === 'acquisitions.md' })
+      challengeUtils.solveIf(challenges.directoryListingChallenge, () => {
+        return file.toLowerCase() === 'acquisitions.md'
+      })
       verifySuccessfulPoisonNullByteExploit(file)
 
       res.sendFile(path.resolve('ftp/', file))
     } else {
       res.status(403)
-      next(new Error('Only .md and .pdf files are allowed!'))
+      next(new Error('You are not authorized to access this file!'))
     }
   }
 
   function verifySuccessfulPoisonNullByteExploit (file: string) {
-    challengeUtils.solveIf(challenges.easterEggLevelOneChallenge, () => { return file.toLowerCase() === 'eastere.gg' })
-    challengeUtils.solveIf(challenges.forgottenDevBackupChallenge, () => { return file.toLowerCase() === 'package.json.bak' })
-    challengeUtils.solveIf(challenges.forgottenBackupChallenge, () => { return file.toLowerCase() === 'coupons_2013.md.bak' })
-    challengeUtils.solveIf(challenges.misplacedSignatureFileChallenge, () => { return file.toLowerCase() === 'suspicious_errors.yml' })
+    challengeUtils.solveIf(challenges.easterEggLevelOneChallenge, () => {
+      return file.toLowerCase() === 'eastere.gg'
+    })
+    challengeUtils.solveIf(challenges.forgottenDevBackupChallenge, () => {
+      return file.toLowerCase() === 'package.json.bak'
+    })
+    challengeUtils.solveIf(challenges.forgottenBackupChallenge, () => {
+      return file.toLowerCase() === 'coupons_2013.md.bak'
+    })
+    challengeUtils.solveIf(challenges.misplacedSignatureFileChallenge, () => {
+      return file.toLowerCase() === 'suspicious_errors.yml'
+    })
 
     challengeUtils.solveIf(challenges.nullByteChallenge, () => {
       return challenges.easterEggLevelOneChallenge.solved || challenges.forgottenDevBackupChallenge.solved || challenges.forgottenBackupChallenge.solved ||
-        challenges.misplacedSignatureFileChallenge.solved || file.toLowerCase() === 'encrypt.pyc'
+                challenges.misplacedSignatureFileChallenge.solved || file.toLowerCase() === 'encrypt.pyc'
     })
   }
 
