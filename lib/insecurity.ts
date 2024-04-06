04b4ac98bb2903b4e69ae659491448dbc48dd689
Gonçalo Pinto <63504894+gpe0@users.noreply.github.com>
2024-04-06 18:31:42 +0100

    Fixed Forged Coupons by introducing a more secure encryption algorithm

--- lib/insecurity.ts
+++ lib/insecurity.ts
@@ -13,6 +13,9 @@ import jws from 'jws'
 import sanitizeHtmlLib from 'sanitize-html'
 import sanitizeFilenameLib from 'sanitize-filename'
 import * as utils from './utils'
+import AES from 'crypto-js/aes'
+import CryptoJS from 'crypto-js'
+
 
 /* jslint node: true */
 // eslint-disable-next-line @typescript-eslint/prefer-ts-expect-error
@@ -20,7 +23,10 @@ import * as utils from './utils'
 import * as z85 from 'z85'
 
 export const publicKey = fs ? fs.readFileSync('encryptionkeys/jwt.pub', 'utf8') : 'placeholder-public-key'
+
 const privateKey = '-----BEGIN RSA PRIVATE KEY-----\r\nMIICXAIBAAKBgQDNwqLEe9wgTXCbC7+RPdDbBbeqjdbs4kOPOIGzqLpXvJXlxxW8iMz0EaM4BKUqYsIa+ndv3NAn2RxCd5ubVdJJcX43zO6Ko0TFEZx/65gY3BE0O6syCEmUP4qbSd6exou/F+WTISzbQ5FBVPVmhnYhG/kpwt/cIxK5iUn5hm+4tQIDAQABAoGBAI+8xiPoOrA+KMnG/T4jJsG6TsHQcDHvJi7o1IKC/hnIXha0atTX5AUkRRce95qSfvKFweXdJXSQ0JMGJyfuXgU6dI0TcseFRfewXAa/ssxAC+iUVR6KUMh1PE2wXLitfeI6JLvVtrBYswm2I7CtY0q8n5AGimHWVXJPLfGV7m0BAkEA+fqFt2LXbLtyg6wZyxMA/cnmt5Nt3U2dAu77MzFJvibANUNHE4HPLZxjGNXN+a6m0K6TD4kDdh5HfUYLWWRBYQJBANK3carmulBwqzcDBjsJ0YrIONBpCAsXxk8idXb8jL9aNIg15Wumm2enqqObahDHB5jnGOLmbasizvSVqypfM9UCQCQl8xIqy+YgURXzXCN+kwUgHinrutZms87Jyi+D8Br8NY0+Nlf+zHvXAomD2W5CsEK7C+8SLBr3k/TsnRWHJuECQHFE9RA2OP8WoaLPuGCyFXaxzICThSRZYluVnWkZtxsBhW2W8z1b8PvWUE7kMy7TnkzeJS2LSnaNHoyxi7IaPQUCQCwWU4U+v4lD7uYBw00Ga/xt+7+UqFPlPVdz1yyr4q24Zxaw0LgmuEvgU5dycq8N7JxjTubX0MIRR+G9fmDBBl8=\r\n-----END RSA PRIVATE KEY-----'
+
+const secretKey = '9RaGUme1+6Y8x)£G<6eNfv8LUeMOgq';
 
 interface ResponseWithUser {
   status: string
@@ -104,14 +110,16 @@ export const userEmailFrom = ({ headers }: any) => {
 
 export const generateCoupon = (discount: number, date = new Date()) => {
   const coupon = utils.toMMMYY(date) + '-' + discount
-  return z85.encode(coupon)
+
+  return AES.encrypt(coupon, secretKey).toString();
 }
 
 export const discountFromCoupon = (coupon: string) => {
   if (coupon) {
-    const decoded = z85.decode(coupon)
-    if (decoded && (hasValidFormat(decoded.toString()) != null)) {
-      const parts = decoded.toString().split('-')
+    let decoded = AES.decrypt(coupon, secretKey)
+    decoded = CryptoJS.enc.Utf8.stringify(decoded);
+    if (decoded && (hasValidFormat(decoded) != null)) { 
+      const parts = decoded.split('-')
       const validity = parts[0]
       if (utils.toMMMYY(new Date()) === validity) {
         const discount = parts[1]
